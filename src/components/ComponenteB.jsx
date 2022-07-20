f8f4065ddcc1ef74c96b37b5ecf08e677715e6cf
MaiBad <95112487+MaiBad@users.noreply.github.com>
2022-07-19 21:38:04 -0500

    Update and rename contact.jsx to ComponenteB.jsx

--- src/components/ComponenteB.jsx
+++ src/components/ComponenteB.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Contact from '../models/contact.class'
 
 const ContactComponent = ({contacto}) => {
+  
+  const [estado, setEstado] = useState(contacto.conectado)
+  
   return (
     <div>
         <h4>Nombre: {contacto.nombre}</h4>
         <h4>Apellido: {contacto.apellido}</h4>
         <h4>Email: {contacto.email}</h4>
-        <h4>Conectado: {contacto.conectado ? 'Contacto En Línea' : 'Contacto no disponible'}</h4>
+        <h4>Conectado: {estado ? 'Contacto En Línea' : 'Contacto no disponible'}</h4>
+        <button onClick={()=>setEstado(!estado)}>{estado ? 'Desconectar' : 'Conectar'}</button>
     </div>
   )
 }
